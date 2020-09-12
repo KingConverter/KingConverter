940836c9f70ebb3680605ad691da660e39d32cde
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-09-12 14:14:05 +0530

    added dropdown and convert button functionality

---
+++ renderer/convert.js
@@ -0,0 +1,56 @@
+var sharp = require("sharp");
+var path = require("path");
+
+const FORMATS = ["png", "jpg", "jpeg", "tiff", "webp"];
+
+var convert = (input_path, destination_format) => {
+  var { dir, name } = path.parse(input_path);
+  console.log(dir);
+  console.log(name);
+  sharp(input_path).toFile(path.join(dir, name + "." + destination_format));
+};
+
+var dropdown = document.getElementById("dest-format-dropdown");
+const fillOptions = input_format => {
+  // handle jpeg and jpg format
+  if (input_format === "jpg" || input_format == "jpeg") {
+    input_format = ["jpeg", "jpg"];
+  }
+
+  // convert to array
+  if (!Array.isArray(input_format)) input_format = [input_format];
+
+  var filtered_formats = FORMATS.filter(element => !input_format.includes(element));
+
+  // Clear all options first
+  var length = dropdown.options.length;
+  for (i = length - 1; i >= 0; i--) {
+    dropdown.options[i] = null;
+  }
+
+  for (var i = 0; i < filtered_formats.length; i++) {
+    var opt = document.createElement("option");
+    opt.value = filtered_formats[i];
+    opt.innerHTML = filtered_formats[i];
+    dropdown.appendChild(opt);
+  }
+  dropdown.disabled = false;
+};
+
+const getCommonFileExtension = filePaths => {
+  if (!filePaths || filePaths.length === 0) return false;
+
+  var ext = path.extname(filePaths[0]);
+  for (var i = 1; i < filePaths.length; i++) {
+    if (path.extname(filePaths[i]) !== ext) return false;
+  }
+
+  console.log(ext);
+  return ext;
+};
+
+module.exports = {
+  convert,
+  fillOptions,
+  getCommonFileExtension,
+};
--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,6 +1,14 @@
+// const { fillOptions } = require("./convert");
+
 const { dialog } = require("electron").remote;
 
-const filePickerDiv = document.getElementById("file-picker");
+const [filePickerDiv, convertButton, destFormatDropdown] = [
+  "file-picker",
+  "convert-button",
+  "dest-format-button",
+].map(id => document.getElementById(id));
+
+var filePaths, destinationFormat;
 
 filePickerDiv.addEventListener("click", e => {
   // Open file selector dialog
@@ -9,11 +17,35 @@ filePickerDiv.addEventListener("click", e => {
     properties: ["openFile", "multiSelections"],
   });
 
-  // If no files have been selected, return
-  if (!filePaths) {
+  var input_extension = getCommonFileExtension(filePaths);
+  console.log(filePaths);
+  console.log(input_extension);
+  if (input_extension) {
+    console.log(filePaths);
+    fillOptions(input_extension);
+    convertButton.disabled = false;
+  } else if (!filePaths || filePaths.length === 0) {
     console.log("No files selected.");
     return;
+  } else if (!input_extension) {
+    // all extensions are not the same
+    // TODO: Show error to user with message:
+    // "Please add files with same format"
+  }
+});
+
+destFormatDropdown.addEventListener("click", e => {
+  destinationFormat = destFormatDropdown.options[destFormatDropdown.selectedIndex].value;
+});
+
+convertButton.addEventListener("click", e => {
+  // default value
+  if (!destinationFormat) destinationFormat = destFormatDropdown.options[0].value;
+
+  for (var i = 0; i < filePaths.length; i++) {
+    convert(filePaths[i], destinationFormat);
   }
 
-  console.log(filePaths);
+  filePaths = undefined;
+  convertButton.disabled = true;
 });
